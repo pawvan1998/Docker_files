48ba9f15d33350d991f8617d22bba530d596a7d0
pawvan1998 <55293135+pawvan1998@users.noreply.github.com>
2024-02-09 11:21:17 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -5,10 +5,10 @@ import numpy as np
 
 st.set_page_config(page_title="Financial Planning Calculator")
 
-st.title("Financial Planning Calculator-December-2022")
+st.title("Financial Planning Calculator-january-2024")
 
-st.header("**Monthly Income-September-2022**")
-st.subheader("Salary-2022")
+st.header("**Monthly Income-Febuary-2024**")
+st.subheader("Salary-2024")
 colAnnualSal, colTax = st.columns(2)
 
 with colAnnualSal:
